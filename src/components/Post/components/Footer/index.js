10988ac2591971e92650fb1eb9c842cc28cf0100
kj6995 <kushagra.me14iitp@gmail.com>
2020-11-09 16:00:03 +0530

    added icons and like functionality

--- src/components/Post/components/Footer/index.js
+++ src/components/Post/components/Footer/index.js
@@ -1,10 +1,45 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {View, Text, TouchableWithoutFeedback} from 'react-native';
 import styles from './styles';
+import ADIcon from 'react-native-vector-icons/AntDesign';
+import FontistoIcon from 'react-native-vector-icons/Fontisto';
+import IoniconsIcon from 'react-native-vector-icons/Ionicons';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
 
-const Footer = ({likesCount, caption, postedAt}) => {
+const Footer = ({likesCount: likesCountProp, caption, postedAt}) => {
+  const [isLike, setIsLike] = useState(false);
+  const [likesCount, setLikesCount] = useState(0);
+
+  const onLikePressed = () => {
+    const amount = isLike ? -1 : 1;
+    setIsLike(!isLike);
+    setLikesCount(likesCount + amount);
+  };
+
+  useEffect(() => {
+    setLikesCount(likesCountProp);
+  }, []);
+
   return (
     <View style={styles.container}>
+      <View style={styles.iconsContainer}>
+        <View style={styles.leftIcons}>
+          <TouchableWithoutFeedback onPress={onLikePressed}>
+            {isLike ? (
+              <ADIcon name="heart" size={25} color={'red'} />
+            ) : (
+              <ADIcon name="hearto" size={25} color={'#545454'} />
+            )}
+          </TouchableWithoutFeedback>
+          <FontistoIcon name="comment" size={21} color={'#545454'} />
+          <IoniconsIcon
+            name="paper-plane-outline"
+            size={26}
+            color={'#545454'}
+          />
+        </View>
+        <FontAwesomeIcon name="bookmark-o" size={23} color={'#545454'} />
+      </View>
       <Text style={styles.likes}>{likesCount} Likes</Text>
       <Text style={styles.caption}>{caption}</Text>
       <Text style={styles.postedAt}>{postedAt}</Text>
--- src/components/Post/components/Footer/styles.js
+++ src/components/Post/components/Footer/styles.js
@@ -1,16 +1,27 @@
 import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create({
+  caption: {
+    margin: 3,
+  },
   container: {
     margin: 5,
+  },
+  iconsContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    padding: 5,
+  },
+  leftIcons: {
+    flexDirection: 'row',
+    width: 120,
+    justifyContent: 'space-between',
   },
   likes: {
     fontWeight: 'bold',
     margin: 3,
   },
-  caption: {
-    margin: 3,
-  },
+
   postedAt: {
     color: '#8c8c8c',
     margin: 3,
